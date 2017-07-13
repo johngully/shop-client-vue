7ea4c6dd68a52c8f9e86e7cf363fc023abc8e1d8
John Gully <john@gully.us>
2017-07-13 09:16:47 -0500

    Updated styles

--- src/components/shared/app-header.vue
+++ src/components/shared/app-header.vue
@@ -4,8 +4,8 @@
     <div class="header__category"><router-link :to="{ name: 'Products' }">Eyeglasses</router-link></div>
     <products-search></products-search>
     <div class="header__cart">
-      <router-link :to="{ name: 'Checkout' }" class="link" type="button">
-        <img src="~patternpack/images/icon-cart.svg" /> ( {{cartCount}} )
+      <router-link :to="{ name: 'Checkout' }" class="link button" type="button">
+        <img src="~patternpack/images/icon-cart.svg" />( {{cartCount}} )
       </router-link>
     </div>
   </header>
