90e5cdf61bb9abf5c8c09598ae0cd1f97ebd80ca
John Gully <john@gully.us>
2018-01-31 20:17:46 -0600

    Modified products service to use graphql

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,16 +1,33 @@
-import axios from 'axios'
-const apiUrl = 'https://62mjnjf07k.execute-api.us-east-1.amazonaws.com/dev'
+import productsQuery from './ProductsQuery.gql'
+import productQuery from './ProductQuery.gql'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+// Create the graphql client
+const apolloClient = new ApolloClient({
+  link: createHttpLink({ uri: 'http://localhost:3030/products' }),
+  cache: new InMemoryCache()
+})
 
 export default {
 
   getProducts: function () {
-    return axios.get(`${apiUrl}/products`)
-      .then(response => response.data)
+    return apolloClient.query({ query: productsQuery })
+      .then((response) => {
+        return response.data.products
+      })
   },
 
   getProduct: function (id) {
-    return axios.get(`${apiUrl}/product/${id}`)
-      .then(response => response.data)
+    return apolloClient.query({
+      query: productQuery,
+      variables: {
+        productId: id
+      }
+    }).then((response) => {
+      return response.data.product
+    })
   }
 
 }
